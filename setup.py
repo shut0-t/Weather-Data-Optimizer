6b6e53ed14790420313c93718af2040656a10477
shut0-t <119462579+shut0-t@users.noreply.github.com>
2024-05-27 06:56:08 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my-csv-optimizer',
+    name='eather-Data-Optimizer',
     version='1.0.0',
     author='Your Name',
     author_email='your@email.com',
