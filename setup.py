e85addc21bc39fc032c4cd1f6b586f1d65525c0f
shut0-t <119462579+shut0-t@users.noreply.github.com>
2024-05-27 06:32:21 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my-csv-optimizer',
+    version='1.0.0',
+    author='Your Name',
+    author_email='your@email.com',
+    description='A tool for optimizing CSV data and fetching weather information.',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'requests',
+        'beautifulsoup4',
+    ],
+)
