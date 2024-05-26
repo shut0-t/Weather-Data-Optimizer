6e6960a6075aa68cf4b49695eea9bf95f7e7598e
shut0-t <119462579+shut0-t@users.noreply.github.com>
2024-05-27 06:59:17 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='eather-Data-Optimizer',
     version='1.0.0',
-    author='Your Name',
-    author_email='your@email.com',
+    author='shut0',
+    author_email='shut0@email.com',
+    license=LICENSE,
     description='A tool for optimizing CSV data and fetching weather information.',
     packages=find_packages(),
     install_requires=[
